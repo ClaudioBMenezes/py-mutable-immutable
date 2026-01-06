6f1477bbe24d016071086d7a4dd41a76239269a1
Claudio Menezes <claudiobmenezes@gmail.com>
2026-01-06 14:27:29 -0300

    por favor verificar correcaop

--- app/main.py
+++ app/main.py
@@ -16,8 +16,9 @@ marks = {
 }
 collection_of_coins = {1, 2, 25}
 
-dicionario = {
-    "mutable": [lucky_number, pi, one_is_a_prime_number, name],
-    "immutable": [my_favourite_films, profile_info, collection_of_coins]
 
+
+sorted_variables = {
+    "mutable": [my_favourite_films, marks, collection_of_coins],
+    "immutable": [lucky_number, pi, one_is_a_prime_number, name, profile_info]
 }
